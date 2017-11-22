5b1b1201b2bfba26373a8e4a7739a607c2d1b820
Neshant Sharma <neshant@Neshants-MacBook-Pro.local>
2017-11-21 20:54:16 -0500

    created eureka service and stock service

--- db-service/src/main/java/com/companyname/dbservice/DbServiceApplication.java
+++ db-service/src/main/java/com/companyname/dbservice/DbServiceApplication.java
@@ -2,9 +2,12 @@ package com.companyname.dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@EnableJpaRepositories(basePackages="com.companyname.dbservice.repository")
+@EnableJpaRepositories(basePackages="com.companyname.dbservice.repository")
 @SpringBootApplication
+@EnableEurekaClient
 public class DbServiceApplication {
 
 	public static void main(String[] args) {
--- stock-service/src/main/java/com/companyname/stock/stockservice/StockServiceApplication.java
+++ stock-service/src/main/java/com/companyname/stock/stockservice/StockServiceApplication.java
@@ -2,9 +2,10 @@ package com.companyname.stock.stockservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class StockServiceApplication {
 
 	public static void main(String[] args) {
