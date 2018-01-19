a1e81cb156073f5040cd65bca0511d9004b26740
neshant <neshantsharma89@gmail.com>
2018-01-19 15:25:28 -0800

    lambda features

--- java8Features/src/lambda/observable/CodeBlock.java
+++ java8Features/src/lambda/observable/CodeBlock.java
@@ -1,11 +1,63 @@
 package lambda.observable;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 public class CodeBlock {
+
+	private String value="Global variable in Class";
+
+
+	
+	
+	
+	
+//	6. Avoid Overloading Methods with Functional Interfaces as Parameters. I did not notice the anomaly here. This may well be wrong.
+
+	public interface Adder {
+	    String add(Function<String, String> f);
+	    void add(Consumer<Integer> f);
+	}
+	 
+	public class AdderImpl implements Adder {
+	 
+	    @Override
+	    public  String add(Function<String, String> f) {
+	        return f.apply("Something ");
+	    }
+	 
+	    @Override
+	    public void add(Consumer<Integer> f) {
+	    	f.accept(10);
+	    }
+	}
+	
+	
+	
 
 	public static void main(String[] args) {
 
 		CodeBlock codeBlock = new CodeBlock();
-
+		
+		
+		AdderImpl adderImpl = codeBlock.new AdderImpl();
+		String str = adderImpl.add(a -> a + " from lambda");
+		
+		adderImpl.add(a -> { String abc =a+" blah lambda";System.out.println(abc);});
+		System.out.println(str);
+		
+		
+//		7. Don’t Treat Lambda Expressions as Inner Classes. 
+//		This example goes to show that the use of this inside the lambda expression means parent or global variable.
+		
+//		whereas its usage inside a normal overridden implementation would mean this is for the local value.
+		System.out.println(codeBlock.scopeExperiment());
+		
+		
+		
+		
+		
+		
 		// http://www.baeldung.com/java-8-lambda-expressions-tips
 		
 //		8.
@@ -23,7 +75,7 @@ public class CodeBlock {
 //	8.5	Use Method References. this is very differnt way of using lambdas.notice we have not defined an input parameter as per the definition.
 		//we have mentioned the class name and the method . here the method toLower case also expects a string input which is same as the interface
 		//method definition also it returns string. Here you can call the methods already implemented from existing classes using double colon notation and 
-		ExecuteThis2 executeThis2 = String::toLowerCase;
+		Foo executeThis2 = String::toLowerCase;
 		
 		
 //		9. Use “Effectively Final” Variables
@@ -42,7 +94,7 @@ public class CodeBlock {
 		executeThis3.methodExec("lols systout ln");
 		
 			
-		System.out.println(executeThis2.methodExec("TOOFAN"));
+		System.out.println(executeThis2.method("TOOFAN"));
 		
 		System.out.println(executeThis.methodExec(6));
 		
@@ -62,6 +114,31 @@ public class CodeBlock {
 	}
 
 	
+	
+	
+//7. supporting method 
+	public String scopeExperiment() {
+	    Foo fooIC = new Foo() {
+	        String value = "Inner class value";
+	 
+	        @Override
+	        public String method(String string) {
+	            return this.value;
+	        }
+	    };
+	    String resultIC = fooIC.method("");
+	 
+	    Foo fooLambda = parameter -> {
+	        String value = "Lambda value";
+	        return this.value;
+	    };
+	    String resultLambda = fooLambda.method("");
+	 
+	    return "Results: resultIC = " + resultIC + 
+	      ", resultLambda = " + resultLambda;
+	}
+	
+	
 	@FunctionalInterface
 	interface ExecuteThis3 {
 
@@ -72,9 +149,9 @@ public class CodeBlock {
 	
 	
 	@FunctionalInterface
-	interface ExecuteThis2 {
+	interface Foo {
 
-		String methodExec(String num);
+		String method(String num);
 
 	}
 
@@ -86,8 +163,6 @@ public class CodeBlock {
 
 
 	}
-	
-	
 	
 	public String testMethod(int num) {
 		if (num > 5)
